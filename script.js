bea1ad6ee2108c8ba3fae5eb87936228b3c34a01
ameera-abed-aldin <ameeraabed369@gmail.com>
2024-12-12 17:03:27 +0200

    built-first-section

---
+++ script.js
@@ -0,0 +1,10 @@
+ 
+  const options = {  
+    strings: ["Welcome to My Portfolio", "IAM FULL STACK DEVELOPER"],  
+    typeSpeed: 70, // typing speed in milliseconds  
+    backSpeed: 25, // backspacing speed  
+    backDelay: 1000, // delay before backspacing  
+    loop: false // loop the typing effect  
+  };  
+
+  const typed = new Typed("#typed-output", options);  
